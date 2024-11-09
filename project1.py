f216d214e178ce160a2aa33e2ff7b36c34b7caa3
GitBon25 <vanka05078@gmail.com>
2024-11-09 16:24:38 +1000

    Update project1.py
    +переключение между окнами
    +вставление ссылки

--- project1.py
+++ project1.py
@@ -1,18 +1,31 @@
-import sys
-from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QComboBox, QPushButton
-from PyQt6.QtCore import Qt
-from pytube import YouTube
-from PyQt6 import uic
+import sys, os, time, requests, pyperclip
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt6 import uic, QtCore
+import pytube
+from threading import Thread
 
 
-class YT_Downloader(QWidget):
+class Window1(QMainWindow):
     def __init__(self):
-        super(YT_Downloader, self).__init__()
+        super(Window1, self).__init__()
         uic.loadUi("project1.ui", self)
-  
+
+        self.url_set.clicked.connect(lambda: self.url_edit.setText(pyperclip.paste()))
+        self.down1.clicked.connect(self.show_new_window)
+
+    def show_new_window(self, checked):
+        self.w2 = Window2()
+        self.w2.show()
+
+
+class Window2(QMainWindow):
+    def __init__(self):
+        super(Window2, self).__init__()
+        uic.loadUi("project2.ui", self)
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    card_form = YT_Downloader()
+    card_form = Window1()
     card_form.show()
     sys.exit(app.exec())
