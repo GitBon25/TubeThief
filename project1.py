c92bd0c3a5f6490f2786b6742f7089b69b1dae06
GitBon25 <vanka05078@gmail.com>
2024-11-17 13:58:54 +1000

    Update project1.py

--- project1.py
+++ project1.py
@@ -1,5 +1,5 @@
 import sys, os, time, requests, pyperclip
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QFileDialog
 from PyQt6 import uic, QtCore
 import pytube
 from threading import Thread
@@ -10,26 +10,34 @@ class Window1(QMainWindow):
         super(Window1, self).__init__()
         uic.loadUi("project1.ui", self)
 
+        self.w2 = Window2()
+
         self.url_set.clicked.connect(lambda: self.url_edit.setText(pyperclip.paste()))
         self.down1.clicked.connect(self.show_new_window)
 
-    def show_new_window(self, checked):
-        self.w2 = Window2()
+    def show_new_window(self):
         self.w2.show()
         self.close()
+
 
 class Window2(QMainWindow):
     def __init__(self):
         super(Window2, self).__init__()
         uic.loadUi("project2.ui", self)
 
+        self.directory.setEnabled(False)
         self.download_button.clicked.connect(self.download)
+        self.browse.clicked.connect(self.getDirectory)
 
-    def download(ytlink):
-        youtubelink = pytube.YouTube(ytlink)
+    def download(self):
+        youtubelink = pytube.YouTube(self.ytlink)
         video = youtubelink.streams.get_highest_resolution()
         video.download()
 
+    def getDirectory(self):                                                    
+        directory = QFileDialog.getExistingDirectory(self, "Выбрать папку" , ".")
+        self.directory.clear
+        self.directory.setText(directory)
 
 
 if __name__ == "__main__":
